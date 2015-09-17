a9aa11d3d5579dcfcfc2ff589a2e0b7be1e8197e
alexandru1985 <alexandru_panzaru@yahoo.com>
2015-09-17 12:46:50 +0300

    proiect

--- application/views/data_view.php
+++ application/views/data_view.php
@@ -12,8 +12,10 @@
             <p><a href="<?php  echo base_url()?>test/add">Add new</a></p>
            
                 <table border ="1">
+                    <tr><td>Id</td><td>Name</td><td>Tel</td><td>City</td><td>State</td><td>Edit</td><td>Delete</td></tr>
                         <?php foreach ($dt as $rows) {
                 ?>  
+                    
                     <tr>
                         <td><?php echo $rows->id ?></td>
                         <td><?php echo $rows->name ?></td>
